42a175e30ba37c688ea18bcb3f355b4bd4c11548
Mike Nitchie <nitchie@careevolution.com>
2015-11-08 17:43:47 -0500

    Use the HomeController for the root (/) route

--- MaintenanceTracker/App_Start/RouteConfig.cs
+++ MaintenanceTracker/App_Start/RouteConfig.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MaintenanceTracker
 {
-    public class RouteConfig
+	public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
@@ -15,8 +11,8 @@ namespace MaintenanceTracker
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                url: "",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
