c3ff8cf3f24a7e191191c32e478a41a18acfc7df
Mike Nitchie <nitchie@careevolution.com>
2015-11-08 17:42:07 -0500

    Added lots of bootstrappy sillyness that visual studio forced on me.

---
+++ MaintenanceTracker/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MaintenanceTracker.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public string Index()
+		{
+			return "Hello, world!";
+		}
+	}
+}
--- MaintenanceTracker/Global.asax.cs
+++ MaintenanceTracker/Global.asax.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MaintenanceTracker
 {
-    public class WebApiApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-    }
+	public class WebApiApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+
+			// Added because Visual Studio told me to after I added an MVC5 controller.
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes( RouteTable.Routes );
+			FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
+			BundleConfig.RegisterBundles( BundleTable.Bundles );
+		}
+	}
 }
