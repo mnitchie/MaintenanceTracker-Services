4bbc9b3043881dc79dcdbee8db1298f8923e779f
Mike Nitchie <nitchie@careevolution.com>
2015-11-08 16:41:15 -0500

    Add stubbed interface to Edmunds API

---
+++ MaintenanceTracker/EdmundsAPI/CarMakeRepository.cs
@@ -0,0 +1,14 @@
+﻿using MaintenanceTracker.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MaintenanceTracker.EdmundsAPI
+{
+	public class CarMakeRepository : ICarMakeRepository
+	{
+		public IEnumerable<CarMake> GetAllMakes()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ MaintenanceTracker/EdmundsAPI/ICarMakeRepository.cs
@@ -0,0 +1,14 @@
+﻿using MaintenanceTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceTracker.EdmundsAPI
+{
+	interface ICarMakeRepository
+	{
+		IEnumerable<CarMake> GetAllMakes();
+	}
+}
