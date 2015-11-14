46446d427ecc24af505a4aec5257aeb04fef5f83
Mike Nitchie <nitchie@careevolution.com>
2015-11-13 19:12:45 -0500

    Allow connection from remote front-end for development.

--- MaintenanceTracker/App_Start/WebApiConfig.cs
+++ MaintenanceTracker/App_Start/WebApiConfig.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MaintenanceTracker
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.EnableCors( new EnableCorsAttribute( "*", "*", "*" ) );
+			// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+
+			/*config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);*/
+
+			config.Routes.MapHttpRoute(
+				name: "CarMake",
+				routeTemplate: "api/carMakes",
+				defaults: new { controller = "CarMake"}
+			);
+		}
+	}
 }
---
+++ MaintenanceTracker/Controllers/CarMakeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MaintenanceTracker.Controllers
+{
+	public class CarMakeController : ApiController
+	{
+		[HttpGet]
+		public IEnumerable<string> GetMakes()
+		{
+			return new string[] { "value1", "value2" };
+		}
+	}
+}
