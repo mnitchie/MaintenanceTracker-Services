d3eeb705fe47b78f8f932ba8f4c7e8ceec280d8d
Mike Nitchie <nitchie@careevolution.com>
2015-11-15 12:14:48 -0600

    Implement repo method to retrieve car makes from Edmunds API

--- MaintenanceTracker/EdmundsAPI/CarMakeRepository.cs
+++ MaintenanceTracker/EdmundsAPI/CarMakeRepository.cs
@@ -1,14 +1,26 @@
-﻿using MaintenanceTracker.Models;
+﻿using MaintenanceTracker.EdmundsAPI.Obj;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace MaintenanceTracker.EdmundsAPI
 {
 	public class CarMakeRepository : ICarMakeRepository
 	{
-		public IEnumerable<CarMake> GetAllMakes()
+		public async Task<IEnumerable<Make>> GetAllMakes()
 		{
-			throw new NotImplementedException();
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri( "http://api.edmunds.com/" );
+			client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+
+			HttpResponseMessage response = await client.GetAsync( "api/vehicle/v2/makes?api_key=''" );
+			response.EnsureSuccessStatusCode();
+
+			var something = JsonConvert.DeserializeObject<CarMakesDTO>( await response.Content.ReadAsStringAsync() );
+			return something.Makes;
 		}
 	}
 }
