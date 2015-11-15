5d006f7731ec18440d7e49a18d35e76cc61f6277
Mike Nitchie <nitchie@careevolution.com>
2015-11-15 12:11:02 -0600

    Camel case JSON responses. Why, .NET. Why?

--- MaintenanceTracker/App_Start/WebApiConfig.cs
+++ MaintenanceTracker/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
-﻿using System.Web.Http;
-using System.Web.Http.Cors;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 
 namespace MaintenanceTracker
 {
@@ -10,13 +11,12 @@ namespace MaintenanceTracker
 			// Web API routes
 			config.MapHttpAttributeRoutes();
 
+			// Camel-case JSON
+			var jsonSettings = config.Formatters.JsonFormatter.SerializerSettings;
+			jsonSettings.Formatting = Formatting.Indented;
+			jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-			/*config.Routes.MapHttpRoute(
-				name: "DefaultApi",
-				routeTemplate: "api/{controller}/{id}",
-				defaults: new { id = RouteParameter.Optional }
-			);*/
-
+			// Set up routes
 			config.Routes.MapHttpRoute(
 				name: "CarMake",
 				routeTemplate: "api/carMakes",
