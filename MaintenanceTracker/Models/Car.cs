a8db9c0d01127384c662104b510e7af0b7d8e1cc
Mike Nitchie <nitchie@careevolution.com>
2015-12-12 16:07:28 -0500

    Adding a car model and Entity Framework stuff.

---
+++ MaintenanceTracker/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaintenanceTracker.Models
+{
+	public class Car
+	{
+		public Guid? Id { get; set; }
+
+		[Required]
+		public string Year { get; set; }
+
+		[Required]
+		public Guid Make { get; set; }
+
+		[Required]
+		public Guid Model { get; set; }
+	}
+}
