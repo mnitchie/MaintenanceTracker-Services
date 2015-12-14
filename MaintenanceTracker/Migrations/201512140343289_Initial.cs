15175abac3c79af9da2f48ccbde822f7f45f12ef
Mike Nitchie <mikenitchie@gmail.com>
2015-12-13 22:46:11 -0500

    Something?

---
+++ MaintenanceTracker/Migrations/201512140343289_Initial.cs
@@ -0,0 +1,26 @@
+namespace MaintenanceTracker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Year = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cars");
+        }
+    }
+}
