17dbfd755145553b846f62cd0ed6647e4be9d0ef
Mike Nitchie <mikenitchie@gmail.com>
2015-12-13 22:38:56 -0500

    Add dbset for cars

--- MaintenanceTracker/DataModels/MaintenanceTrackerContext.cs
+++ MaintenanceTracker/DataModels/MaintenanceTrackerContext.cs
@@ -1,8 +1,10 @@
-﻿using System.Data.Entity;
+﻿using MaintenanceTracker.Classes;
+using System.Data.Entity;
 
 namespace MaintenanceTracker.DataModels
 {
 	public class MaintenanceTrackerContext : DbContext
 	{
+		public DbSet<Car> Cars { get; set; }
 	}
 }
